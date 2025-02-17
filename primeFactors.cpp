2fc772e05f7d7a28c72a0795c2df9225d99a25f7
Prakhar Srivastava <142735678+prakshark@users.noreply.github.com>
2025-02-17 09:37:24 +0530

    Add files via upload

---
+++ primeFactors.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    vector<int>ans;
+    for(int i = 2; i*i <= n; i++) {
+        while(n % i == 0) {
+            ans.push_back(i);
+            n /= i;
+        }
+    }
+    if(n > 1) ans.push_back(n);
+
+    return 0;
+}
